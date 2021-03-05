077d33b3cd2fd5ae4d7e1cc7302d0d7b2cbb0958
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2021-03-04 22:26:27 -0500

    validate SHA regex and uploads

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
 use std::path::PathBuf;
+
+use lazy_static::lazy_static;
+
 use sha2::Digest;
 
-use rocket::{Response, http::Status, routes, tokio};
-use rocket::response::Result;
+use regex::Regex;
+
 use rocket::data::ToByteUnit;
+use rocket::response::Result;
+use rocket::tokio::fs::File;
 use rocket::tokio::io::AsyncReadExt;
 use rocket::tokio::io::AsyncWriteExt;
+use rocket::{http::Status, routes, Response};
+
+use tempfile::NamedTempFile;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new("[0-9a-fA-F]{64}").unwrap();
+}
 
 // TODO
-// Path can escape?
-// SHA filename
-// Assert SHA equal
-// Better Error handling
+// - Path can escape?
+// - Assert SHA equal
+// - Better Error handling
+// - Remove rocket friendly default
+//   - CLI output
+//   - Override rocket default error catcher
 
 #[get("/<sha>")]
 async fn get_cas<'a>(sha: String) -> Result<'a> {
+    if !RE.is_match(sha.as_str()) {
+        return Err(Status::BadRequest);
+    }
+
     let p: PathBuf = [".", "data", sha.as_str()].iter().collect();
-    match tokio::fs::File::open(p).await {
+    match File::open(p).await {
         Ok(file) => Ok(Response::build().sized_body(None, file).finalize()),
         Err(_) => Err(Status::InternalServerError),
     }
@@ -25,16 +45,18 @@ async fn get_cas<'a>(sha: String) -> Result<'a> {
 
 #[put("/<sha>", data = "<data>")]
 async fn put_cas<'a>(sha: String, data: rocket::data::Data) -> Result<'a> {
-    let p: PathBuf = [".", "data", sha.as_str()].iter().collect();
+    if !RE.is_match(sha.as_str()) {
+        return Err(Status::BadRequest);
+    }
+
     let mut stream = data.open(512.mebibytes());
     let mut bbuf = bytes::BytesMut::with_capacity(4096);
     let mut sum = sha2::Sha256::new();
-    let mut file = tokio::fs::File::create(p).await.map_err(|err| {
-            println!("open {}", err);
-            Status::InternalServerError
-        })?; 
-    
-    loop { 
+
+    let tempfile = NamedTempFile::new().map_err(|_| Status::InternalServerError)?;
+    let mut file = File::from_std(tempfile.reopen().map_err(|_| Status::InternalServerError)?);
+
+    loop {
         let cnt = stream.read_buf(&mut bbuf).await.map_err(|err| {
             println!("read {}", err);
             Status::InternalServerError
@@ -50,8 +72,16 @@ async fn put_cas<'a>(sha: String, data: rocket::data::Data) -> Result<'a> {
         bbuf.clear();
     }
 
-    println!("{:x}", sum.finalize());
+    let temppath = tempfile.into_temp_path();
 
+    if sha.to_ascii_lowercase() != hex::encode(sum.finalize()) {
+        return Err(Status::BadRequest);
+    }
+
+    let p: PathBuf = [".", "data", sha.as_str()].iter().collect();
+    temppath
+        .persist_noclobber(p)
+        .map_err(|_| Status::InternalServerError)?;
     Ok(Response::build().status(Status::Created).finalize())
 }
 
